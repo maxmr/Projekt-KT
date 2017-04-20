19b8b5b928305b4e7d187f6e277d3cc83b75ee36
maxmr <maxmr92@gmail.com>
2017-04-20 14:33:31 +0200

    fsm header clear

--- ESP/ESP/FSM.h
+++ ESP/ESP/FSM.h
@@ -1,7 +1,7 @@
 
 #ifndef FSM_H
 #define FSM_H
-#
+
 
 
 #if defined(ARDUINO) && ARDUINO >= 100
@@ -13,13 +13,7 @@
 #endif
 
 //States
-#define Startup 1
-#define In_Game 2
-#define Active 3
-#define Shot 4
-#define Out_of_Game 5
-#define End 6
-#define Failmode7 
+
 
 int State_Machine(void);
 
