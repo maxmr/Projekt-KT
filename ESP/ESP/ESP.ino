bcaa047eb7e9483499d93adaed5e2345d15596d8
Philipp Grundner <3phil3@web.de>
2017-04-20 13:16:32 +0200

    hallo max test

--- ESP/ESP/ESP.ino
+++ ESP/ESP/ESP.ino
@@ -10,6 +10,7 @@ const char* host = "10.11.12.1";
 const int Port = 8888;
 
 //hallo phil
+//hallo max
 // Initialize the OLED display using Wire library test
 SSD1306  display(0x3c, SDA, SCL);  //Adaruit HUZZAH: GPIO 4 und 5
 
