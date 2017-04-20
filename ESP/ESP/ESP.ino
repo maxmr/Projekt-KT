58c87f039623bbb0914b16cb5b89ea8f5f292bc1
fraenky94 <fr.kaiser@gmx.de>
2017-04-20 12:10:09 +0200

    test commit

--- ESP/ESP/ESP.ino
+++ ESP/ESP/ESP.ino
@@ -10,7 +10,7 @@ const char* host = "10.11.12.1";
 const int Port = 8888;
 
 
-// Initialize the OLED display using Wire library
+// Initialize the OLED display using Wire library test
 SSD1306  display(0x3c, SDA, SCL);  //Adaruit HUZZAH: GPIO 4 und 5
 
 void setup()
