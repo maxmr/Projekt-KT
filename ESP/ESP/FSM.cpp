4e563fc6bdd48064fffd99c26265a558c4555b3e
maxmr <maxmr92@gmail.com>
2017-04-22 20:02:20 +0200

    1. Änderung der States in die korrekte form "-" als Bindeglied, nicht "_", sonst error beim String parsen. 2. einfügen der oled_lage zum regelmäßigen updaten, sonst volle prozessorauslastung durch oled 3. Kommentare geändert und unnötige includes entfernt

--- ESP/ESP/FSM.cpp
+++ ESP/ESP/FSM.cpp
@@ -1,31 +1,50 @@
 #include "FSM.h"
-#include "oled_func.h"
-#include "images.h"
+
 
 void State_Machine(void)
 {
-	if (strcmp(str_ClientState, "PRE_GAME") == 0)
+	if (strcmp(str_ClientState, "PRE-GAME") == 0)
 	{
-		//update_oled_pregame(); Hier evtl Team Bekanntgabe? Oder Punkteanzeige (also wie viel pkt pro kill oder so?)
-	}
-	else if (strcmp(str_ClientState, "STARTING_GAME") == 0)
-	{
-		//10Sekunden OLED runterzaehlen -> Funktion von Philipp??				
-	else if (strcmp(str_ClientState, "IN_GAME"))
-	{
-		update_oled_active(); 
-		if (i_GotHit != 0)
+		if (oled_flag == 1)
 		{
-			update_oled_hit(); 
+			//update_oled_pregame(); Hier evtl Team Bekanntgabe? Oder Punkteanzeige (also wie viel pkt pro kill oder so?)
+			update_oled_wifidata();
 		}
 	}
-	else if (strcmp(str_ClientState, "END_GAME") == 0)
+	else if (strcmp(str_ClientState, "STARTING-GAME") == 0)
 	{
-		// funktion zur Bekanntgabe der Points bzw. Kill/Ratio?
+		if (oled_flag == 1)
+		{
+			//update_oled_startinggame();
+			//10Sekunden OLED runterzaehlen -> Funktion von Philipp??	
+		}
+	}
+
+	else if (strcmp(str_ClientState, "IN-GAME"))
+	{
+		if (oled_flag == 1)
+		{
+			if (i_GotHit != 0)
+			{
+				//update_oled_hit();
+			}
+			else
+			{
+				//update_oled_active();
+			}
+			oled_flag = 0;
+		}
+	}
+	else if (strcmp(str_ClientState, "END-GAME") == 0)
+	{
+		if (oled_flag == 1)
+		{
+			// funktion zur Bekanntgabe der Points bzw. Kill/Ratio?
+			//update_oled_endgame();
+		}
 	}
 	else
 	{
-		// Funktion das Verbindung abgebrochen ist? 
-		// oder versuchen neue verbindung aufzubauen?
+	// kein gültiger state? evlt error_function();
 	}
 }
