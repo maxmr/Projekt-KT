603fda18caf89a0faa5998770521d47a7376c7d0
Daniel Sander <dsander@hmedu.onmicrosoft.com>
2017-04-27 13:13:32 +0200

    Funktionen angepasst. Achtung! Neue Fonts! OLEDDisplayFonts.h aus dem LRZ einfuegen!

--- ESP/ESP/oled_func.cpp
+++ ESP/ESP/oled_func.cpp
@@ -9,32 +9,39 @@
 // Initialize the OLED display using Wire library test
 SSD1306  display(0x3c, SDA, SCL);  //Adaruit HUZZAH: GPIO 4 und 5
 
-int mode = 0, lifes = 0, munition = 0, points = 0;
+char	playerStr[] = { "Player:" }, modeStr[] = { "Mode:" }, dBmStr[] = { "dBm" }, rankStr[] = { "Rank:" },
+pointStr[] = { "Points:" }, timeStr[] = { "MM:SS" };
 
-char	playerStr[] = { "Player:" }, modeStr[] = { "Mode:" }, dBmStr[] = { "dBm" }, lifeStr[] = { "Lifes:" },
-muniStr[] = { "Munition:" }, pointStr[] = { "Points:" };
+void update_oled_start()
+{
+	display.clear();
+	display.setTextAlignment(TEXT_ALIGN_LEFT);
+	display.setFont(ArialMT_Plain_16);
+	display.drawString(48, 0, modeStr);
+	display.drawString(34, 5, str_GameMode);
+	display.setTextAlignment(TEXT_ALIGN_CENTER);
+	display.setFont(Arimo_40);
+	display.drawString(64, 22, String(i_CountdownValue));
+	display.display();
+}
 
 void update_oled_active(void)
 {
 	display.clear();
-	display.setTextAlignment(TEXT_ALIGN_LEFT);//hallo
+	display.setTextAlignment(TEXT_ALIGN_LEFT);
 	display.setFont(ArialMT_Plain_10);
 	display.drawString(0, 0, playerStr);
-	display.drawString(35, 0, String(i_ClientID));
-	display.drawString(48, 0, modeStr);
-	display.drawString(80, 0, str_GameMode);
-	//display.drawString(85, 0, "RSSI:");
-	display.drawString(98, 0, RSSI_Str);
+	display.drawString(33, 0, String(i_ClientID));
 	display.drawString(106, 0, dBmStr);
+	display.drawString(94, 0, String(RSSI_Str));
 	display.drawHorizontalLine(0, 49, 128);
 
-	display.drawString(0, 17, lifeStr);
-	display.drawString(48, 17, muniStr);
+	display.drawString(94, 18, rankStr);
 	display.setFont(ArialMT_Plain_16);
-	display.drawString(0, 29, String(lifes));
-	display.drawString(48, 29, String(munition));
-
+	display.drawString(0, 23, timeStr);
+	display.drawString(94, 30, String(i_ClientRank));
 	display.drawHorizontalLine(0, 15, 128);
+
 	display.setFont(ArialMT_Plain_10);
 	display.drawString(0, 53, pointStr);
 	display.drawString(35, 53, String(i_ClientPoints));
@@ -45,17 +52,5 @@ void update_oled_hit(void)
 {
 	display.clear();
 	display.drawXbm(0, 0, Skull_Logo_width, Skull_Logo_height, Skull_Logo_bits);
-	display.display();
-}
-
-void update_countdown()
-{
-	display.clear();
-	display.setTextAlignment(TEXT_ALIGN_LEFT);
-	display.setFont(ArialMT_Plain_16);
-	display.drawString(34, 5, str_GameMode);
-	display.setTextAlignment(TEXT_ALIGN_CENTER);
-	display.setFont(Arimo_40);
-	display.drawString(64, 22, String(i_CountdownValue));
 	display.display();
 }
